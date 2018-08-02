999fc67c8b1adea1db91507c567853778067efe8
Jinfijizo23 <1009508@student.sae.edu.au>
2018-08-02 15:47:46 +1000

    Initial Commit
    Initial Commit to MindSpace VR PRoject.

---
+++ Assets/CubeChanger.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CubeChanger : MonoBehaviour {
+
+    private Renderer R;
+    public Text FunctionStatus;
+    private float Statusnumber;
+    public GameObject ObjectToSpawn;
+    public AudioSource Boop;
+    public float Rotatespeed = 10f;
+
+	// Use this for initialization
+	void Start ()
+    {
+        R = GetComponent<Renderer>();
+    }
+
+    public void Randomjob(Vector3 location)
+    {
+        var rndnum = Random.Range(0, 3);
+        Statusnumber = rndnum;
+        FunctionStatus.text = "Function: "+ Statusnumber;
+        if(rndnum == 0)
+        {
+            Function1(location);
+        }
+        if(rndnum == 1)
+        {
+            Function2();
+        }
+        if(rndnum == 2)
+        {
+            Function3();
+        }
+    }
+
+     void Function1(Vector3 spawnPoint)
+    {
+        R.material.color = Color.red;
+        Instantiate(ObjectToSpawn, spawnPoint, Quaternion.identity);
+    }
+
+    void Function2()
+    {
+        R.material.color = Color.green;
+        Boop.Play();
+    }
+    void Function3()
+    {
+        R.material.color = Color.blue;
+        transform.Rotate(0, Rotatespeed * Time.deltaTime, 0);
+
+    }
+}
---
+++ Assets/Laser.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Laser : MonoBehaviour {
+ 
+    private LineRenderer LZR;
+    public Text Status;
+    public Text TriggerSelected;
+
+    //Use this for initialization
+
+   void Start ()
+    {
+
+       LZR = GetComponent<LineRenderer>();
+
+       LZR.material.color = Color.red;
+
+    }
+
+    // Update is called once per frame
+    public void Update ()
+    {
+        if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger))
+        {
+            //raycast stuff goes here
+            ShootRaycast();
+        }
+        
+
+	}
+
+
+    public void ShootRaycast()
+    {
+        RaycastHit hit;
+        Debug.DrawRay(transform.position, Vector3.forward, Color.magenta, 0.1f);
+        if (Physics.Raycast(transform.position, transform.forward, out hit))
+        {
+            if (hit.collider)
+            {
+                LZR.SetPosition(1, new Vector3(0, 0, hit.distance));
+                Status.text = hit.collider.gameObject.name + " hit! Distance: " + hit.distance;
+                if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger))
+                {
+                    TriggerSelected.text = "Trigger clicked";
+                    hit.collider.gameObject.GetComponent<CubeChanger>().Randomjob(hit.point);
+                }
+            }
+            else
+            {
+                LZR.SetPosition(1, new Vector3(0, 0, 1000));
+            }
+        }
+
+    }
+}
+
+// New Audio: Weightless By Marcaroni Union (Discuss liscensing and implement ingame)
